36702c3c1d51a0bdd6677abf29cb9d443d9d2a18
David <dahe_22@alu.uabcs.mx>
2024-12-03 15:17:24 -0700

    Cambios iconos y colores transacciones dinamicas

--- src/shared/components/transactions-preview.js
+++ src/shared/components/transactions-preview.js
@@ -1,36 +1,29 @@
 import "./transactions-preview.css";
 export const AddTransactionPreview = (description, date, type, amount) => {
     let iconSrc;
-    let styleIcon;
     switch (type) {
         case 'transfer':
             iconSrc = 'transfer-icon.svg';
-            styleIcon = 'transfer-icon';
             break;
         case 'payment':
             iconSrc = 'payment-sheet-icon.svg';
-            styleIcon = 'payment-icon';
             break;
         case 'income':
             iconSrc = 'income-icon.svg';
-            styleIcon = 'deposit-icon';
             break;
-
         case 'withdrawal':
             iconSrc = 'withdrawal-icon.svg';
-            styleIcon = 'withdrawal-icon';
             break;
         default:
             iconSrc = 'income-icon.svg';
-            styleIcon = 'deposit-icon';
             break;
     }
 
     const contentTransaction = document.getElementById('transactions-preview')
     const content = `
-    <div class="transaction-card">
+    <div class="transaction-card ${type}">
         <div class="icon-container">
-            <img id="transaction-icon" class="${styleIcon}" src="${iconSrc}" title="${type}">
+            <img id="transaction-icon" class="deposit-icon" src="${iconSrc}" title="${type}">
         </div>
         <div class="transaction-details">
             <span>${description}</span>
