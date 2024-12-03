add32d4fd76da4bef9669ba0c2df506d03b975e1
YahirRadilla <yahirradi09@gmail.com>
2024-12-03 01:16:31 -0700

    home estilos y componente titulo creado

---
+++ src/features/page/pages/home.js
@@ -0,0 +1,24 @@
+import '../../../shared/components/card.css'
+import './home.css'
+import { Card } from '../../../shared/components/card.js';
+import { Title } from '../../../shared/components/title.js';
+import { HandleApi } from '../../../shared/utils/api.js'
+import { monitorUserSession } from '../../../shared/utils/helpers/verify-token.js'
+
+export const getUser = async () => {
+
+    const path = window.location.pathname;
+
+
+
+    await monitorUserSession()
+    const encodedId = localStorage.getItem('userId');
+    const userId = atob(encodedId);
+
+
+    console.log(userId)
+
+    Card()
+    Title('Yahir')
+
+}
