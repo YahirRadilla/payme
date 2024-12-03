613f0a850729975359bb9380277d6153cfeb4ac6
David <dahe_22@alu.uabcs.mx>
2024-12-02 18:59:47 -0700

    Componente de edit-profile

---
+++ src/features/page/pages/components/edit-profile.js
@@ -0,0 +1,29 @@
+export const EditProfile = () => {
+
+    const mainSection = document.getElementById('main-section');
+    mainSection.innerHTML = `
+        <div>
+            <label class="titles-content">Edit Profile Information</label>
+            <div class="inline-inputs">
+                <div class="input-container">
+                    <label for="name" class="name">First Name:</label>
+                    <input required id="name" name="firstName" placeholder="Enter your name" type="text" class="input">
+                </div>
+                <div class="input-container">
+                    <label for="lastname" class="name">Last Name:</label>
+                    <input id="lastname" name="lastName" placeholder="Enter your last name" type="text" class="input">
+                </div>
+                <div class="input-container">
+                    <label for="email" class="name">E-Mail:</label>
+                    <input required id="email" name="emal" placeholder="Enter your email" type="email" class="input">
+                </div>
+                <div class="input-container">
+                    <label for="phone" class="name">Phone Number:</label>
+                    <input required id="phone" name="phone" placeholder="Enter your phone number" type="number"
+                        class="input">
+                </div>
+            </div>
+            <input class="input" type="submit" value="Update Profile">
+        </div>
+    `;
+};
