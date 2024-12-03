b91bc720733b01f3eff479ddc72dc3f7529e7b7f
FmMau <mfonseca_22@alu.uabcs.mx>
2024-12-03 11:57:18 -0700

    titulo de servicios agregado

--- src/shared/components/card-services.js
+++ src/shared/components/card-services.js
@@ -4,6 +4,7 @@ export const AddCardServices = () => {
 
     containerServices.innerHTML = `
     <div>
+        <h1 class="card-title">Services</h1>
         <div class="services-card">
             <ul class="services-list">
             <li class="service-item">
