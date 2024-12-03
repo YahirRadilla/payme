719ddd1a4ff5e7c6ede694e2098e24f897cf7620
FmMau <mfonseca_22@alu.uabcs.mx>
2024-12-03 03:40:38 -0700

    pagina de cards modificada

---
+++ src/shared/components/card-form.js
@@ -0,0 +1,34 @@
+export const AddCardForm = () => {
+    const container = document.getElementById('card-container');
+
+    container.innerHTML = `
+        <div>
+            <h1 class="card-title">Add Card</h1>
+            <div class="card-container">
+                <form>
+                    <div class="form-group">
+                        <div class="form-column">
+                            <label for="card-type">Card Type</label>
+                            <input type="text" id="card-type" placeholder="Classic">
+                        </div>
+                        <div class="form-column">
+                            <label for="card-name">Name On Card</label>
+                            <input type="text" id="card-name" placeholder="My Cards">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-column">
+                            <label for="card-number">Card Number</label>
+                            <input type="text" id="card-number" placeholder="**** **** **** ****">
+                        </div>
+                        <div class="form-column">
+                            <label for="expiry-date">Expiration Date</label>
+                            <input type="text" id="expiry-date" placeholder="25 January 2025">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn">Add Card</button>
+                </form>
+            </div>
+        </div>
+    `;
+};
