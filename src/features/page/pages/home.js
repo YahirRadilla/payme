9517130be5ed76cc010f71b5b7c614b9b7b3fea7
YahirRadilla <yahirradi09@gmail.com>
2024-12-03 13:38:28 -0700

    empty card agregada

--- src/features/page/pages/home.js
+++ src/features/page/pages/home.js
@@ -1,25 +1,28 @@
 import '../../../shared/components/card.css'
 import './home.css'
 import { Card } from '../../../shared/components/card.js';
+import { EmptyCard } from '../../../shared/components/empty-card.js';
 import { Title } from '../../../shared/components/title.js';
 import { HandleApi } from '../../../shared/utils/api.js'
 import { monitorUserSession } from '../../../shared/utils/helpers/verify-token.js'
 import { AddCardServices } from '../../../shared/components/card-services.js';
 
+
 export const getHome = async () => {
 
     const path = window.location.pathname;
-    
+
     await monitorUserSession()
     const encodedId = localStorage.getItem('userId');
     const userId = atob(encodedId);
-    
-    
+
+
     //const si = await HandleApi.getTransactions({ id: userId })
-    
+
     //console.log(si)
-    
+
     Card()
+    EmptyCard()
     Title('Yahir')
     AddCardServices()
 
