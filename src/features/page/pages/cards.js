7ac184ea53fdbdf629d1b2455ea80979e0a5af80
FmMau <mfonseca_22@alu.uabcs.mx>
2024-12-04 06:48:36 -0700

    Ajustes en estilos de tarjetas y contenedores, y se agregó el componente EmptyCard en la lógica de tarjetas

--- src/features/page/pages/cards.js
+++ src/features/page/pages/cards.js
@@ -2,11 +2,13 @@ import '../../../shared/components/card.css'
 import { Card } from '../../../shared/components/card.js';
 import { AddCardForm } from '../../../shared/components/card-form.js';
 import { AddCardServices } from '../../../shared/components/card-services.js';
+import { EmptyCard } from '../../../shared/components/empty-card.js';
 
 export const getCards = () => {
 
     Card(1234123412341234, '12/23', 123, 'John Doe', 1000);
     AddCardForm(1, '12/23', 123, 'John Doe', 1000);
     AddCardServices(1, '12/23', 123, 'John Doe', 1000);
-
+    EmptyCard();
+    EmptyCard();
 };
