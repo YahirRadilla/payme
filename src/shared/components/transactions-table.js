2f3445d1f293ae0ca0042d6b28ed0d4e081accd7
FmMau <mfonseca_22@alu.uabcs.mx>
2024-12-03 18:36:46 -0700

    pagina de transacciones responsiva

--- src/shared/components/transactions-table.js
+++ src/shared/components/transactions-table.js
@@ -5,86 +5,86 @@ export const AddTransactions = () => {
 
     containerTransactions.innerHTML = `
         <div class="transactions-card">
-            <div class="transactions-scroll">
-                <table class="transactions-table">
-                    <thead>
-                        <tr>
-                            <th>Description</th>
-                            <th>Folio</th>
-                            <th>Type</th>
-                            <th>Card</th>
-                            <th>Date</th>
-                            <th>Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Transfer</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="negative-amount">-$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Income</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="positive-amount">+$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Service Payment</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="negative-amount">-$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Utilities</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="negative-amount">-$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Income</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="positive-amount">+$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Income</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="positive-amount">+$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Utilities</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="negative-amount">-$14.23</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor</td>
-                            <td>#1234567890</td>
-                            <td>Income</td>
-                            <td>1234 ****</td>
-                            <td>14 Nov 2024, 4:43 PM</td>
-                            <td class="positive-amount">+$14.23</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <div class="transactions-scroll">
+        <table class="transactions-table">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Folio</th>
+                    <th>Type</th>
+                    <th>Card</th>
+                    <th>Date</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Transfer</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="negative-amount">-$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Income</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="positive-amount">+$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Service Payment</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="negative-amount">-$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Utilities</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="negative-amount">-$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Income</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="positive-amount">+$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Income</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="positive-amount">+$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Utilities</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="negative-amount">-$14.23</td>
+                </tr>
+                <tr>
+                    <td data-label="Description">Lorem ipsum dolor</td>
+                    <td data-label="Folio">#1234567890</td>
+                    <td data-label="Type">Income</td>
+                    <td data-label="Card">1234 ****</td>
+                    <td data-label="Date">14 Nov 2024, 4:43 PM</td>
+                    <td data-label="Amount" class="positive-amount">+$14.23</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
     `;
 };
