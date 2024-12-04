878153bd1e18916209bc37457c32cb2a564e7c57
FmMau <mfonseca_22@alu.uabcs.mx>
2024-12-03 22:10:03 -0700

    Modal de servicios

--- src/shared/components/modals/service-content.js
+++ src/shared/components/modals/service-content.js
@@ -1,37 +1,35 @@
 export const serviceContent = () => `
-    <header class="modal__header">
-                <h2 class="modal__title">
-                    Service data
-                </h2>
-                <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
-    </header>
-    <div class="modal-content-content">
-        <div class="modal__content">
-            <form class="transfer-inputs">
-                <label for="transfer-card-select" class="name">Source Card</label>
-                <select required name="transfer-card-select">                    
-                    <option disabled>Select a type</option>                
-                    <option value="income">Income</option>
-                    <option value="expense">Expense</option>
-                </select>  
+<header class="modal__header">
+    <h2 class="modal__title">Payment of services</h2>
+    <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+</header>
+<div class="modal-content-content">
+    <div class="modal__content">
+        <form class="transfer-inputs">
+            <label for="transfer-card-select" class="name">Source Card</label>
+            <select required name="transfer-card-select" id="transfer-card-select">
+                <option disabled selected>**** 1234</option>
+                <!-- Agrega más opciones si es necesario -->
+            </select>
 
-                <div class="input-container">
-                    <label for="email" class="name">E-Mail</label>
-                    <input required id="email" name="email" placeholder="Enter your email" type="email" class="input">
-                </div>
+            <label for="service-select" class="name">Service</label>
+            <select required name="service-select" id="service-select">
+                <option disabled selected>Electricity</option>
+                <!-- Agrega más opciones si es necesario -->
+            </select>
 
-                <div class="input-container">
-                    <label for="amount" class="name">Amount</label>
-                    <input required id="amount" name="amount" placeholder="$70,000" type="number"
-                        class="input">
-                </div>
+            <div class="input-container">
+                <label for="reference" class="name">Reference</label>
+                <input required id="reference" name="reference" value="012345678901" type="text" class="input" readonly>
+            </div>
 
-                <div class="input-container">
-                    <label for="message" class="name">Message (Optional)</label>
-                    <input id="message" name="message" placeholder="Your message here" type="text" class="input">
-                </div>
+            <div class="input-container">
+                <label for="amount" class="name">Amount to pay</label>
+                <input required id="amount" name="amount" value="$70,000" type="text" class="input" readonly>
+            </div>
 
-                <input class="input" type="submit" value="Make transfer">
-            </form>
-        </div>
-    </div>`
+            <input class="input" type="submit" value="Pay now">
+        </form>
+    </div>
+</div>
+`
