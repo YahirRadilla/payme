711c723e4b5936b30122a7d883c7bd123dbf5bad
FmMau <mfonseca_22@alu.uabcs.mx>
2024-12-04 02:43:21 -0700

    cambios en user settings

--- src/features/page/pages/user-settings.js
+++ src/features/page/pages/user-settings.js
@@ -6,35 +6,26 @@ export const UserSettings = () => {
     const editButton = document.getElementById("show-edit-profile");
     const changeButton = document.getElementById("show-change-password");
     const deleteButton = document.getElementById("show-delete-account");
+
     EditProfile();
+
     document.addEventListener("click", (event) => {
-        if(!event.target.classList.contains("buttons-sections")) return;
+        if (!event.target.classList.contains("buttons-sections")) return;
 
-        const button = changeButton.classList.contains("active-section");
-        const button1 = editButton.classList.contains("active-section");
-        const button2 = deleteButton.classList.contains("active-section");
+        [editButton, changeButton, deleteButton].forEach(button => {
+            button.classList.remove("active-section");
+            button.style.color = "#242424";
+        });
 
-        if(button) {
-            changeButton.classList.remove("active-section");
-        }else if(button1) {
-            editButton.classList.remove("active-section");
-        }else if(button2) {
-            deleteButton.classList.remove("active-section");
-        }
+        event.target.classList.add("active-section");
+        event.target.style.color = "#515DEF";
 
-        if(event.target.innerHTML === "Change password") {
-            event.target.classList.add("active-section");
+        if (event.target === changeButton) {
             ChangePassword();
-        }
-        else if(event.target.innerHTML === "Edit personal information") {
-
-            event.target.classList.add("active-section");
+        } else if (event.target === editButton) {
             EditProfile();
-        }else if(event.target.innerHTML === "Delete my account") {
-
-            event.target.classList.add("active-section");
+        } else if (event.target === deleteButton) {
             DeleteAccount();
         }
-
     });
 };
